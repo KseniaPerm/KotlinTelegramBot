0bc2846a761ec02f4c79c0c5918be26811ba4330
KPermyakova <k.e.permyakova@yandex.ru>
2025-07-08 13:53:53 +0500

    KTB-05-create-a-basic-application-structure

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,16 +2,50 @@ package org.example
 
 import java.io.File
 
-fun main() {
-    val wordsFile = File("words.txt")
-    wordsFile.createNewFile()
-    wordsFile.writeText("hello привет\n")
-    wordsFile.appendText("dog собака\n")
-    wordsFile.appendText("cat кошка\n")
-    val words = wordsFile.readLines()
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswerCount: Int? = 0,
+)
 
-    for (i in words) {
-        println(i)
+fun loadDictionary(): MutableList<Word> {
+    val wordsFile = File("words.txt")
+    val lines: List<String> = wordsFile.readLines()
+    val dictionary: MutableList<Word> = mutableListOf()
+    for (line in lines) {
+        val line = line.split("|")
+        val word = Word(original = line[0], translate = line[1], correctAnswerCount = line[2].toIntOrNull() ?: 0)
+        dictionary.add(word)
         Thread.sleep(1000)
+    }
+    return dictionary
+}
+
+fun main() {
+    val dictionary = loadDictionary()
+    while (true) {
+        println("Введите число:\n1 - Учить слова\n2 - Статистика\n0 - Выход")
+        val number = readln().toInt()
+        println()
+
+        when (number) {
+            1 -> {
+                println("Учить слова")
+            }
+
+            2 -> {
+                println("Статистика")
+            }
+
+            0 -> {
+                println("Выход")
+                break
+            }
+
+            else -> {
+                println("Введите число 1, 2 или 0")
+            }
+        }
+        println()
     }
 }
