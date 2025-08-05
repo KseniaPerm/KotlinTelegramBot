528519f13d6d6fc192aa1573e32c1bedf959bbbb
KPermyakova <k.e.permyakova@yandex.ru>
2025-08-05 13:06:33 +0500

    KTB-09-code-optimization

---
+++ src/main/kotlin/LearnWordsTrainer.kt
@@ -0,0 +1,73 @@
+package org.example
+
+import java.io.File
+
+data class Statistics(
+    val totalCount: Int,
+    val learnedCount: Int,
+    val percent: Double,
+)
+
+data class Question(
+    val variants: List<Word>,
+    val correctAnswer: Word,
+)
+
+class LearnWordsTrainer {
+
+    private var question: Question? = null
+    private val dictionary = loadDictionary()
+
+    fun getStatistics(): Statistics {
+        val totalCount = dictionary.count()
+        val learnedCount = dictionary.count { it.correctAnswerCount >= CORRECT_ANSWER }
+        val percent = ((learnedCount.toDouble() / totalCount.toDouble()) * 100)
+        return Statistics(totalCount, learnedCount, percent)
+    }
+
+    fun getNextQuestion(): Question? {
+        val notLearnedList = dictionary.filter { it.correctAnswerCount < CORRECT_ANSWER }
+        if (notLearnedList.isEmpty()) return null
+        val questionWords = notLearnedList.shuffled().take(NOT_LEARNED_WORDS)
+        val correctAnswer = questionWords.random()
+
+        question = Question(
+            variants = questionWords,
+            correctAnswer = correctAnswer,
+        )
+        return question
+    }
+
+    fun checkAnswer(userAnswerIndex: Int?): Boolean {
+        return question?.let {
+            val correctAnswerId = it.variants.indexOf(it.correctAnswer)
+            if (correctAnswerId == userAnswerIndex) {
+                it.correctAnswer.correctAnswerCount++
+                saveDictionary(dictionary)
+                true
+            } else {
+                false
+            }
+        } ?: false
+    }
+
+    private fun loadDictionary(): MutableList<Word> {
+        val wordsFile = File("words.txt")
+        val lines: List<String> = wordsFile.readLines()
+        val dictionary: MutableList<Word> = mutableListOf()
+        for (line in lines) {
+            val line = line.split("|")
+            val word = Word(original = line[0], translate = line[1], correctAnswerCount = line[2].toIntOrNull() ?: 0)
+            dictionary.add(word)
+        }
+        return dictionary
+    }
+
+    private fun saveDictionary(words: MutableList<Word>) {
+        val wordsFile = File("words.txt")
+        wordsFile.writeText("")
+        for (word in words) {
+            wordsFile.appendText("${word.original}|${word.translate}|${word.correctAnswerCount}\n")
+        }
+    }
+}
