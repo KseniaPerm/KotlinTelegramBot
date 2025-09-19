2cd478510734248d47cf7468188cd6c6cec38cf3
KPermyakova <k.e.permyakova@yandex.ru>
2025-09-19 15:19:17 +0500

    KTB-21 Migrate to Kotlinx.Serialization

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,49 +1,120 @@
 package org.example
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+@Serializable
+data class Update(
+    @SerialName("update_id")
+    val updateId: Long,
+    @SerialName("message")
+    val message: Message? = null,
+    @SerialName("callback_query")
+    val callbackQuery: CallbackQuery? = null,
+)
+
+@Serializable
+data class Response(
+    @SerialName("result")
+    val result: List<Update>,
+)
+
+@Serializable
+data class Message(
+    @SerialName("text")
+    val text: String,
+    @SerialName("chat")
+    val chat: Chat,
+)
+
+@Serializable
+data class CallbackQuery(
+    @SerialName("data")
+    val data: String,
+    @SerialName("message")
+    val message: Message? = null,
+)
+
+@Serializable
+data class Chat(
+    @SerialName("id")
+    val id: Long,
+)
+
+@Serializable
+data class SendMessageRequest(
+    @SerialName("chat_id")
+    val chatId: Long?,
+    @SerialName("text")
+    val text: String,
+    @SerialName("reply_markup")
+    val replyMarkup: ReplyMarkup? = null,
+)
+
+@Serializable
+data class ReplyMarkup(
+    @SerialName("inline_keyboard")
+    val inlineKeyboard: List<List<InlineKeyboard>>,
+)
+
+@Serializable
+data class InlineKeyboard(
+    @SerialName("callback_data")
+    val callbackData: String,
+    @SerialName("text")
+    val text: String,
+)
+
+
 fun main(args: Array<String>) {
 
     val botToken = args[0]
-    var updateId = 0
+    var lastUpdateId = 0L
     val telegramBotService = TelegramBotService(botToken)
-    val updateIdRegex: Regex = "\"update_id\":([0-9]+)[,}]".toRegex()
-    val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
-    val chatIdRegex: Regex = "\"chat\":\\{\"id\":(\\d+)".toRegex()
-    val dataRegex: Regex = "\"data\":\"(.+?)\"".toRegex()
+
+    val json = Json {
+        ignoreUnknownKeys = true
+    }
+
     val trainer = LearnWordsTrainer()
 
     while (true) {
         Thread.sleep(2000)
-        val updates: String = telegramBotService.getUpdates(updateId)
-        println(updates)
+        val responseString: String = telegramBotService.getUpdates(lastUpdateId)
+        println(responseString)
+        val response: Response = json.decodeFromString(responseString)
+        val updates = response.result
+        val firstUpdate = updates.firstOrNull() ?: continue
+        val updateId = firstUpdate.updateId
+        lastUpdateId = updateId + 1
 
-        val newUpdateId = updateIdRegex.find(updates)?.groups?.get(1)?.value?.toIntOrNull() ?: continue
-        updateId = newUpdateId + 1
-
-        val text = messageTextRegex.find(updates)?.groups?.get(1)?.value?.decodeUnicode()
+        val text = firstUpdate.message?.text
         println(text)
 
-        val chatId: Long = chatIdRegex.find(updates)?.groups?.get(1)?.value?.toLongOrNull() ?: continue
-        val data = dataRegex.find(updates)?.groups?.get(1)?.value?.decodeUnicode()
+        val chatId = firstUpdate.message?.chat?.id ?: firstUpdate.callbackQuery?.message?.chat?.id
+        val data = firstUpdate.callbackQuery?.data
 
         when {
             text?.lowercase() == MENU || text == START -> {
-                telegramBotService.sendMenu(botToken, chatId)
+                telegramBotService.sendMenu(json, botToken, chatId)
             }
 
             data?.lowercase() == STATISTICS -> {
                 val trainerStat = trainer.getStatistics()
                 telegramBotService.sendMessage(
+                    json, botToken,
                     chatId, "Всего слов: ${trainerStat.totalCount}, Выучено: ${trainerStat.learnedCount}," +
                             " Статистика: ${trainerStat.percent}"
                 )
             }
 
             data?.lowercase() == LEARN_WORDS -> {
-                telegramBotService.checkNextQuestionAndSend(trainer, telegramBotService, chatId)
+                telegramBotService.checkNextQuestionAndSend(json, trainer, telegramBotService, chatId)
             }
 
             data?.lowercase() == MENU -> {
-                telegramBotService.sendMenu(botToken, chatId)
+                telegramBotService.sendMenu(json, botToken, chatId)
             }
 
             data?.startsWith(CALLBACK_DATA_ANSWER_PREFIX) ?: false -> {
@@ -53,22 +124,19 @@ fun main(args: Array<String>) {
 
                 val isCorrect = trainer.checkAnswer(userAnswerIndex)
                 if (isCorrect) {
-                    telegramBotService.sendMessage(chatId, "Правильно")
+                    telegramBotService.sendMessage(json, botToken, chatId, "Правильно!")
                 } else {
                     val wordToTranslate = lastQuestion?.correctAnswer?.original
                     val correctAnswer = lastQuestion?.correctAnswer?.translate
-                    telegramBotService.sendMessage(chatId, "Неправильно! $wordToTranslate - это $correctAnswer")
+                    telegramBotService.sendMessage(
+                        json,
+                        botToken,
+                        chatId,
+                        "Неправильно! $wordToTranslate - это $correctAnswer"
+                    )
                 }
-                telegramBotService.checkNextQuestionAndSend(trainer, telegramBotService, chatId)
+                telegramBotService.checkNextQuestionAndSend(json, trainer, telegramBotService, chatId)
             }
         }
-    }
-}
-
-fun String.decodeUnicode(): String {
-    return this.replace("""\\u([0-9a-fA-F]{4})""".toRegex()) { matchResult ->
-        val hexCode = matchResult.groupValues[1]
-        val codePoint = hexCode.toInt(16)
-        String(Character.toChars(codePoint))
     }
 }
